ada40b1153eafc255187d5197db1d8ef232b3262
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2024-01-13 14:13:30 +0200

    guestbook

--- workshopcamp.near/GuestBook.jsx
+++ workshopcamp.near/GuestBook.jsx
@@ -32,7 +32,7 @@ const content = (
     <div className="hero min-h-screen bg-primary">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold text-base-content">BOS workshop</h1>
+          <h1 className="text-5xl font-bold text-base-content">BOS Workshop</h1>
           <p className="mb-4 text-base-content">
             Welcome to the GuestBook, add your message for everyone to see!
           </p>
@@ -49,6 +49,7 @@ const content = (
               }
             ></textarea>
           </div>
+
           <button className="btn btn-primary mb-4" onClick={postMessage}>
             Add a message
           </button>
--- workshopcamp.near/HelloWorld.jsx
+++
@@ -1,1 +0,0 @@
-return <div>Hello World3</div>;
